eee3bebf4127ca6b8cfe285f07438df11e0a902e
Brian Zakariya <zakariyabrian@gmail.com>
2025-02-27 20:17:00 +0700

    Uji coba middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/authController");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
@@ -8,5 +9,10 @@ router.post("/register", registerUser);
 
 // Route Login pengguna
 router.post("/login", loginUser);
+
+// Route proteksi contoh (Dashboard)
+router.get("/dashboard", protect, (req, res) => {
+  res.json({ message: `Halo, ${req.user.name}! Selamat datang di dashboard` });
+});
 
 module.exports = router;
